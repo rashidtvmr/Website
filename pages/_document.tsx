b86e7b5240ce035526fc5742261cc27a13adbb00
matthewmckeller <mattmckeller@gmail.com>
2021-05-05 16:26:48 -0500

    add functionality to the _document.tsx file to inject a stylesheet element into the page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,39 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, {Html, Head, Main, NextScript, DocumentContext} from 'next/document'
+import {ServerStyleSheet} from "styled-components";
 
 export default class MyDocument extends Document {
-  render() {
+
+
+    // Server Side Rendering related context handling
+    static async getInitialProps(ctx: DocumentContext) {
+        // This is so that styled components to work with server side rendering
+        const sheet = new ServerStyleSheet()
+        const originalRenderPage = ctx.renderPage
+
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) =>
+                        sheet.collectStyles(<App {...props} />),
+                })
+
+            const initialProps = await Document.getInitialProps(ctx)
+            console.log("_document", {initialProps});
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            }
+        } finally {
+            sheet.seal()
+        }
+    }
+
+    render() {
     return (
       <Html lang="en">
         <Head />
