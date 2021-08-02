c0b93518ba8e439aab0864f40ae1aad515d70387
rashidtvmr <rashidtvmr@gmail.com>
2021-08-02 16:28:31 +0530

    final touch complete

--- components/ui/containers/page-sections/about-us/TimelineSection.tsx
+++ components/ui/containers/page-sections/about-us/TimelineSection.tsx
@@ -56,53 +56,17 @@ function TimelineSection() {
     <div>
       <Container>
         <TimeLine__>
-          <div className="aboutus__mobile_curve_1">
+          {/* <div className="aboutus__mobile_curve_1">
             <img src="assets/about-us/mobile_curve.svg" alt="" />
-          </div>
+          </div> */}
 
-          <div className="aboutus_curve__round_1">
+          {/* <div className="aboutus_curve__round_1">
             <img src="assets/about-us/round.svg" alt="" />
           </div>
           <div className="aboutus_curve__round_2">
             <img src="assets/about-us/round.svg" alt="" />
-          </div>
-          <div className="aboutus__curved_line__verticle">
-            <img
-              src="/assets/about-us/line_1.svg"
-              height="500px"
-              width="500px"
-            />
-          </div>
-          <div className="aboutus__curved_line__horizontal">
-            <img
-              src="/assets/about-us/line_horizontal.svg"
-              height="500px"
-              width="500px"
-            />
-          </div>
-          <div className="aboutus__curved_line__verticle_final">
-            <img
-              src="/assets/about-us/line_final.svg"
-              height="500px"
-              width="500px"
-            />
-          </div>
-          <div className="aboutus__curved_profile_back">
-            <div style={{ position: "relative" }}>
-              <div className="aboutus__curved_profile">
-                <img
-                  src="/assets/about-us/profile.svg"
-                  height="219px"
-                  width="219px"
-                />
-              </div>
-            </div>
-            <img
-              src="/assets/about-us/profile_back.svg"
-              height="350px"
-              width="350px"
-            />
-          </div>
+          </div> */}
+
           <div>
             <Grid container spacing={2}>
               <Grid item xs={12} md={4} style={{ position: "relative" }}>
@@ -143,29 +107,27 @@ function TimelineSection() {
                         Hard work, growth, creativity, and honesty
                       </div>
                     </div>
-                    <div className="left-card__icon">
+                    <div
+                      className="left-card__icon"
+                      style={{ marginTop: isSmallScreen ? 10 : 0 }}
+                    >
                       <img src="/assets/about-us/Values.svg" alt="" />
                     </div>
                   </Card>
                 </div>
               </Grid>
               <Grid item xs={12} md={8}>
-                <div className="aboutus_leadership">
-                  <div>OUR</div>
-                  <div className="bold">LEADERSHIP</div>
-                </div>
                 <Timeline align={isSmallScreen ? "left" : "right"}>
                   <TimelineItem style={{ marginTop: 20 }}>
                     <TimelineOppositeContent className="timeline__title">
-                      <OppsiteText>
-                        Software Architect and Consultant
-                      </OppsiteText>
+                    Software Architect and Consultant
                     </TimelineOppositeContent>
                     <TimelineSeparator>
                       <TimelineDot
                         style={{
                           backgroundColor: "transparent",
                           boxShadow: "unset",
+                          zIndex: 999999,
                         }}
                       >
                         <img
@@ -173,31 +135,63 @@ function TimelineSection() {
                           width="35px"
                           height="35px"
                         />
+                        <div className="aboutus_curve">
+                          <div className="aboutus_leadership">
+                            <div>OUR</div>
+                            <div className="bold">LEADERSHIP</div>
+                          </div>
+                          {isSmallScreen ? (
+                            <img src="assets/about-us/line_mobile.svg" alt="" />
+                          ) : (
+                            <img src="assets/about-us/line.svg" alt="" />
+                          )}
+                          <div className="aboutus-ceo">
+                            <div className="aboutus-ceo__name">
+                              Matthew McKeller
+                            </div>
+                            <div className="aboutus-ceo__text">CEO</div>
+                          </div>
+                          <div className="aboutus__curved_profile_back">
+                            <div style={{ position: "relative" }}>
+                              <div className="aboutus__curved_profile">
+                                <img
+                                  src="/assets/about-us/profile.svg"
+                                  height="219px"
+                                  width="219px"
+                                />
+                              </div>
+                            </div>
+                            <img
+                              src="/assets/about-us/profile_back.svg"
+                              height="350px"
+                              width="350px"
+                            />
+                          </div>
+                        </div>
                       </TimelineDot>
                       <TimelineConnector
-                        style={{ backgroundColor: "#805DA7", width: 5 }}
+                        style={{ backgroundColor: "#805DA7", width: 0 }}
                       />
                     </TimelineSeparator>
                     <TimelineContent className="timeline__para">
-                      <BIgText>
-                        Agile software development consultant and software
-                        architect for MDU Resources Group Inc. a fortune 1000
-                        company. Introduced Agile Development, Agile Project
-                        Management, and Agile Roadmapping while rebuilding, and
-                        continuously deploying end-of-life applications from the
-                        ground up.
-                      </BIgText>
+                      Agile software development consultant and software
+                      architect for MDU Resources Group Inc. a fortune 1000
+                      company. Introduced Agile Development, Agile Project
+                      Management, and Agile Roadmapping while rebuilding, and
+                      continuously deploying end-of-life applications from the
+                      ground up.
                     </TimelineContent>
                   </TimelineItem>
                   <TimelineItem style={{ marginTop: 20 }}>
                     <TimelineOppositeContent className="timeline__title">
-                      <OppsiteText>Senior Application Developer</OppsiteText>
+                      Senior Application Developer
                     </TimelineOppositeContent>
                     <TimelineSeparator>
                       <TimelineDot
                         style={{
                           backgroundColor: "transparent",
-                          boxShadow:'unset'
+                          boxShadow: "unset",
+                          zIndex: 999999,
                         }}
                       >
                         <img
@@ -207,29 +201,26 @@ function TimelineSection() {
                         />
                       </TimelineDot>
                       <TimelineConnector
-                        style={{ backgroundColor: "#805DA7", width: 5 }}
+                        style={{ backgroundColor: "#805DA7", width: 0 }}
                       />
                     </TimelineSeparator>
                     <TimelineContent className="timeline__para">
-                      <BIgText>
-                        Senior web application developer partnering with a team
-                        of 25 in an agile scrum environment for LexisNexis Risk
-                        Solutions we developed the Risk Intelligence Network, a
-                        high-priority fraud platform for the U.S.
-                      </BIgText>
+                      Senior web application developer partnering with a team of
+                      25 in an agile scrum environment for LexisNexis Risk
+                      Solutions we developed the Risk Intelligence Network, a
+                      high-priority fraud platform for the U.S.
                     </TimelineContent>
                   </TimelineItem>
                   <TimelineItem style={{ marginTop: 20 }}>
                     <TimelineOppositeContent className="timeline__title">
-                      <OppsiteText>
-                        Director of Software Development
-                      </OppsiteText>
+                    Director of Software Development
                     </TimelineOppositeContent>
                     <TimelineSeparator>
                       <TimelineDot
                         style={{
                           backgroundColor: "transparent",
                           boxShadow: "unset",
+                          zIndex: 999999,
                         }}
                       >
                         <img
@@ -239,16 +230,14 @@ function TimelineSection() {
                         />
                       </TimelineDot>
                       <TimelineConnector
-                        style={{ backgroundColor: "#805DA7", width: 5 }}
+                        style={{ backgroundColor: "#805DA7", width: 0 }}
                       />
                     </TimelineSeparator>
                     <TimelineContent className="timeline__para">
-                      <BIgText>
-                        Started and directed a software development department
-                        at an 8-figure student housing company and eventually
-                        grew the department from just one to lead a team of 5
-                        software developers.
-                      </BIgText>
+                      Started and directed a software development department at
+                      an 8-figure student housing company and eventually grew
+                      the department from just one to lead a team of 5 software
+                      developers.
                     </TimelineContent>
                   </TimelineItem>
                 </Timeline>
